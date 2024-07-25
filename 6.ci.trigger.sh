c279c3418be759b74a782b99b19670613174ad4f
gnuhub <gnuhub@gmail.com>
2024-07-25 22:33:50 +0800

    Update 6.ci.trigger.sh

--- 6.ci.trigger.sh
+++ 6.ci.trigger.sh
@@ -12,28 +12,36 @@ sudo apt-get upgrade -y
 sudo apt-get install -y tzdata
 sudo ln -sfv /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 date
- 
-if [ ! -z $OPENOS365_DOWN_NAME ];then
-	down_name=${OPENOS365_DOWN_NAME}
-	cd ~/
-	git clone -b ${GITHUB_REF_NAME} git@github.com:${down_name}.git build
-	cd build
+
+if [ ! -z $OPENOS365_DOWN_NAME_01 ];then
+	cd ~
+	target_name=down_01
+	git clone -b ${GITHUB_REF_NAME} --depth 5 git@github.com:${OPENOS365_UP_NAME_01}.git $target_name
+	cd $target_name
 	echo "${GITHUB_REPOSITORY}:${GITHUB_REF_NAME}:${GITHUB_RUN_NUMBER}:$(date)" >> ci.txt
 	p2 "${GITHUB_REPOSITORY}:${GITHUB_REF_NAME}:${GITHUB_RUN_NUMBER}:$(date)"
 	cd ~
-	rm -rf build
-	date
+	rm -rf $target_name
 fi
 
-if [ ! -z $OPENOS365_DOWN_NAME2 ];then
-	down_name=${OPENOS365_DOWN_NAME2}
-	cd ~/
-	git clone -b ${GITHUB_REF_NAME} git@github.com:${down_name}.git build
-	cd build
+if [ ! -z $OPENOS365_DOWN_NAME_02 ];then
+	cd ~
+	target_name=down_02
+	git clone -b ${GITHUB_REF_NAME} --depth 5 git@github.com:${OPENOS365_UP_NAME_02}.git $target_name
+	cd $target_name
 	echo "${GITHUB_REPOSITORY}:${GITHUB_REF_NAME}:${GITHUB_RUN_NUMBER}:$(date)" >> ci.txt
 	p2 "${GITHUB_REPOSITORY}:${GITHUB_REF_NAME}:${GITHUB_RUN_NUMBER}:$(date)"
 	cd ~
-	rm -rf build
-	date
+	rm -rf $target_name
 fi
 
+if [ ! -z $OPENOS365_DOWN_NAME_03 ];then
+	cd ~
+	target_name=down_03
+	git clone -b ${GITHUB_REF_NAME} --depth 5 git@github.com:${OPENOS365_UP_NAME_03}.git $target_name
+	cd $target_name
+	echo "${GITHUB_REPOSITORY}:${GITHUB_REF_NAME}:${GITHUB_RUN_NUMBER}:$(date)" >> ci.txt
+	p2 "${GITHUB_REPOSITORY}:${GITHUB_REF_NAME}:${GITHUB_RUN_NUMBER}:$(date)"
+	cd ~
+	rm -rf $target_name
+fi
