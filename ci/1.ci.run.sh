afd334c290fc4adeac6982fa526a593cb66c897d
gnuhub <gnuhub@gmail.com>
2024-07-25 20:57:56 +0800

    Update 1.ci.run.sh

--- ci/1.ci.run.sh
+++ ci/1.ci.run.sh
@@ -14,7 +14,7 @@ export GIT_CLONE_PROTECTION_ACTIVE=false
 if [ ! -z $OPENOS365_UP_NAME_01 ];then
 	cd ~
 	target_name=up_01
-	git clone -b ${GITHUB_REF_NAME} --depth 5 git@github.com:${OPENOS365_UP_NAME}.git $target_name
+	git clone -b ${GITHUB_REF_NAME} --depth 5 git@github.com:${OPENOS365_UP_NAME_01}.git $target_name
 	cd $target_name
 	./github.runner.sh
 	cd ~
@@ -24,7 +24,7 @@ fi
 if [ ! -z $OPENOS365_UP_NAME_02 ];then
 	cd ~
 	target_name=up_02
-	git clone -b ${GITHUB_REF_NAME} --depth 5 git@github.com:${OPENOS365_UP_NAME}.git $target_name
+	git clone -b ${GITHUB_REF_NAME} --depth 5 git@github.com:${OPENOS365_UP_NAME_02}.git $target_name
 	cd $target_name
 	./github.runner.sh
 	cd ~
@@ -34,7 +34,7 @@ fi
 if [ ! -z $OPENOS365_UP_NAME_03 ];then
 	cd ~
 	target_name=up_03
-	git clone -b ${GITHUB_REF_NAME} --depth 5 git@github.com:${OPENOS365_UP_NAME}.git $target_name
+	git clone -b ${GITHUB_REF_NAME} --depth 5 git@github.com:${OPENOS365_UP_NAME_03}.git $target_name
 	cd $target_name
 	./github.runner.sh
 	cd ~
